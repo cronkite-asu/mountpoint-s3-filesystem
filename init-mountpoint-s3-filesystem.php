ac9c8bb777d63ce0b4dbe88cf3385dd9bdbe2869
Jeremy Leggat <jleggat@asu.edu>
2024-09-04 16:34:10 -0700

    Create init-mountpoint-s3-filesystem.php

---
+++ init-mountpoint-s3-filesystem.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Mountpoint S3 Filesystem
+Description: Write files to AWS S3 as a directly mounted filesystem.
+Author: Jeremy Leggat
+Version: 1.0
+*/
+
+require __DIR__ . '/mountpoint-s3-filesystem/mountpoint-s3-filesystem.php';
