d4dfcc4b987f6efc8e4b0f21d575410c03cfd810
Jeremy Leggat <jleggat@asu.edu>
2024-09-04 16:48:53 -0700

    Delete public class and hooks

--- mountpoint-s3-filesystem/includes/class-mountpoint-s3-filesystem.php
+++ mountpoint-s3-filesystem/includes/class-mountpoint-s3-filesystem.php
@@ -94,7 +94,6 @@ class Mountpoint_S3_Filesystem {
 
 		$this->load_dependencies();
 		$this->set_locale();
-		$this->define_admin_hooks();
 		$this->define_public_hooks();
 		$this->set_filesystem();
 
@@ -200,21 +199,6 @@ class Mountpoint_S3_Filesystem {
 
 	}
 
-	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_admin_hooks() {
-
-		$plugin_admin = new Mountpoint_S3_Filesystem_Admin( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
-	}
 
 	/**
 	 * Register all of the hooks related to the public-facing functionality
@@ -231,7 +215,6 @@ class Mountpoint_S3_Filesystem {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
-
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
 	 *
