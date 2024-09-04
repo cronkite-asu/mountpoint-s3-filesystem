9bdf2037d0287e09b6dba5113a79a2abc1b97d9b
Jeremy Leggat <jleggat@asu.edu>
2024-09-04 16:50:59 -0700

    Update class-mountpoint-s3-filesystem.php

--- mountpoint-s3-filesystem/includes/class-mountpoint-s3-filesystem.php
+++ mountpoint-s3-filesystem/includes/class-mountpoint-s3-filesystem.php
@@ -134,13 +134,6 @@ class Mountpoint_S3_Filesystem {
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wp-filesystem-mountpoint-s3.php';
 
-
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-mountpoint-s3-filesystem-public.php';
-
 		require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-base.php';
 
 		$this->loader = new Mountpoint_S3_Filesystem_Loader();
