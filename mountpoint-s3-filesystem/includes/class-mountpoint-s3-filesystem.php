75313dc2a6358a9591f7755c31cf193ce5b30b3d
Jeremy Leggat <jleggat@asu.edu>
2024-09-05 15:36:49 -0700

    Update class-mountpoint-s3-filesystem.php

--- mountpoint-s3-filesystem/includes/class-mountpoint-s3-filesystem.php
+++ mountpoint-s3-filesystem/includes/class-mountpoint-s3-filesystem.php
@@ -270,7 +270,7 @@ class Mountpoint_S3_Filesystem {
 	 * @since     1.0.0
 	 * @return    string    The filesystem method.
 	 */
-	public function get_filesystem_method() {
+	public function get_filesystem_method($method, $args, $context, $allow_relaxed_file_ownership) {
 		error_log(__FUNCTION__ . ": filesystem_method = $this->filesystem_method");
 		return $this->filesystem_method; // The Mountpoint_S3 base class transparently handles using the direct filesystem as well as Mountpoint S3.
 	}
