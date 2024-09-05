3c08979ac89a1ef0fc356d4158bdef082e742743
Jeremy Leggat <jleggat@asu.edu>
2024-09-05 14:10:40 -0700

    Update class-wp-filesystem-mountpoints3.php

--- mountpoint-s3-filesystem/includes/class-wp-filesystem-mountpoints3.php
+++ mountpoint-s3-filesystem/includes/class-wp-filesystem-mountpoints3.php
@@ -30,6 +30,7 @@ class WP_Filesystem_MountpointS3 extends WP_Filesystem_Base {
 	 * @param array $dependencies Array that contains an instance of `WP_Filesystem_MountpointS3_Uploads` and `WP_Filesystem_Direct`.
 	 */
 	public function __construct( $dependencies ) {
+		error_log(__CLASS__);
 		$this->method = 'mountpoints3';
 		$this->errors = new WP_Error();
 
