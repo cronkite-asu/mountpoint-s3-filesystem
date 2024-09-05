643ad7f4d68bbf49806303748ceaea4d1d0f944c
Jeremy Leggat <jleggat@asu.edu>
2024-09-04 17:06:58 -0700

    Update class-mountpoint-s3-filesystem.php

--- mountpoint-s3-filesystem/includes/class-mountpoint-s3-filesystem.php
+++ mountpoint-s3-filesystem/includes/class-mountpoint-s3-filesystem.php
@@ -144,10 +144,6 @@ class Mountpoint_S3_Filesystem {
 			$this->loader->add_filter( 'filesystem_method', $this, 'get_filesystem_method', PHP_INT_MAX );
 		}
 
-		// ensure we always upload with year month folder layouts
-		add_filter( 'pre_option_uploads_use_yearmonth_folders', function () {
-			return '1';
-		} );
 		$this->loader->add_filter( 'request_filesystem_credentials', $this, 'get_filesystem_credentials', PHP_INT_MAX, 3 );
 		$this->loader->add_filter( 'sanitize_file_name', $this, 'sanitize_filename' );
 
